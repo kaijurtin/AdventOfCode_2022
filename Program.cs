8e17fdc2b3488dac33e8712e05cc6b1178bac20a
Kai Jurtin <kai@jurtin.de>
2022-11-30 13:44:35 +0100

    create new repo

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+
+namespace AdventOfCodeStartProject
+{
+    static class AdventOfCode
+    {
+        //this field contains whats written in your input.txt
+        //each line in the list of strings represents one line in the txt file
+        //copy the data you get on the adventofcode website into the txt file and start coding!
+        private static List<string> _inputData;
+
+        static void Main(string[] args)
+        {
+            //EXAMPLE CODE
+            //hit F5 to run it
+
+            //read data from input.txt and write it into inputdata field
+            readInput();
+            //var intlist = _inputData.Select(i => int.Parse(i)).ToList();
+            //lets write the contents of the input.txt into the console
+
+            //Console.WriteLine("Content of input.txt:");
+            //_inputData.ForEach(i => Console.WriteLine(i));
+            //Console.WriteLine($"--END OF FILE--{Environment.NewLine}");
+            //List<int> _inputDataINT = _inputData.ConvertAll(item => int.Parse(item));
+
+            //     8,0 -> 0,8
+            List<Point> line = new List<Point>(); 
+
+            foreach(string i in _inputData)
+            {
+                i.Split(" -> " ).ToList();
+                Console.WriteLine(i);
+
+            }
+
+            
+            //keep console open
+            Console.WriteLine("Hit any key to close this window...");
+                Console.ReadKey();
+            
+        }
+        private static void readInput()
+        {
+            _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
+        }
+    }
+}
