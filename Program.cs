c697bf3641a9c27a29e150abb598e2ac4b4e6850
Kai Jurtin <kai@jurtin.de>
2022-12-03 07:13:47 +0100

    day3 round1

--- Program.cs
+++ Program.cs
@@ -27,58 +27,49 @@ namespace AdventOfCodeStartProject
             //_inputData.ForEach(i => Console.WriteLine(i));
             //Console.WriteLine($"--END OF FILE--{Environment.NewLine}");
             //List<int> _inputDataINT = _inputData.ConvertAll(item => int.Parse(item));
-            int points = 0;
-            int total = 0;
-            //var intList = _inputData.Where(i => !string.IsNullOrEmpty(i)).Select(i => int.Parse(i)).ToList();
-            //var intList = _inputData.Where(i => !string.IsNullOrEmpty(i)).Select(i => int.Parse(i)).ToList();
+
+            
+            
+            var total = 0;
             foreach (string s in _inputData)
             {
-                switch (s)
-	            {
-
-		            case "A X":
-                        points = 0 + 3;
-                        break;
-                        case "A Y":
-                        points = 3 + 1;
-                        break;
-                        case "A Z":
-                        points = 6 + 2;
-                        break;
-                        case "B X":
-                        points = 0 + 1;
-                        break;
-                        case "B Y":
-                        points = 3 + 2;
-                        break;
-                        case "B Z":
-                        points = 6 + 3;
-                        break;
-                        case "C X":
-                        points = 0 + 2;
-                        break;
-                        case "C Y":
-                        points = 3 + 3;
-                        break;
-                        case "C Z":
-                        points = 6 + 1;
-                        break;
-
-                    default: points = 0;
-                break;
-	            }
-            total += points;
-
+                
+                var firstHalfLength = (int)(s.Length / 2);
+                var secondHalfLength = s.Length - firstHalfLength;
+                var splitstring = new[]
+                    {
+                        s.Substring(0, firstHalfLength),
+                        s.Substring(firstHalfLength, secondHalfLength)
+                    };
+                var listOfCharsInFirstHalf = splitstring[0].Distinct().ToList();
+                
+                foreach (char c in listOfCharsInFirstHalf)
+                {
+                    if (splitstring[1].Contains(c))
+                    {
+                        Console.WriteLine("common Letter: " + c);
+                        var value = findValue(c);
+                        Console.WriteLine("value: " + value);
+                        total += value;
+                        
+                        
+                        Console.WriteLine("total: " + total);
+                    }
+                }
             }
 
-            Console.WriteLine("total: " + total);
-            
-            
-            
+
+
+
             //keep console open
             Console.WriteLine("Hit any key to close this window...");
                 Console.ReadKey();
             
+        }
+        private static int findValue(char c)
+        {
+            var valueString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return valueString.IndexOf(c) + 1 ;
         }
         private static void readInput()
         {
