38ff26abe8bf6e557f0f763293ef3f63dac3b806
Kai Jurtin <kai@jurtin.de>
2022-12-04 10:49:42 +0100

    day4 round1

--- Program.cs
+++ Program.cs
@@ -26,48 +26,55 @@ namespace AdventOfCodeStartProject
             //Console.WriteLine("Content of input.txt:");
             //_inputData.ForEach(i => Console.WriteLine(i));
             //Console.WriteLine($"--END OF FILE--{Environment.NewLine}");
+            
             //List<int> _inputDataINT = _inputData.ConvertAll(item => int.Parse(item));
 
             
             
             var total = 0;
-            for (int i = 0; i < _inputData.Count; i=i+3)
-            {
-                var one = _inputData[i];
-                var two = _inputData[i + 1];    
-                var three = _inputData[i + 2];
-
-                var listOfCharsInOne = one.Distinct().ToList();
-                foreach (char c in listOfCharsInOne)
-                {
-                    if (two.Contains(c) && three.Contains(c))
-                    {
-                        Console.WriteLine("common Letter: " + c);
-                        var value = findValue(c);
-                        Console.WriteLine("value: " + value);
-                        total += value;
+            var first = 0;
+            var last = 0;
 
 
-                        Console.WriteLine("total: " + total);
+            foreach (string line in _inputData)
+	        {
+                var pairs = line.Split(',');
+                var pairOne = pairs[0].Split('-').ToList();
+                var pairTwo = pairs[1].Split('-').ToList();
+
+                List<int> one = pairOne.ConvertAll(i => int.Parse(i));  
+                List<int> two = pairTwo.ConvertAll(i => int.Parse(i));  
+
+                
+
+                if(one.First() <= two.First() && one.Last() >= two.Last())
+                    { 
+                    total++;
+                    first++;
+                    //Console.WriteLine("first pair contains second pair fully");
+                    continue;
                     }
-                }
 
-            }
+                if(two.First() <= one.First() && two.Last() >= one.Last())
+                    {
+                    total++;
+                    last++;
+                    //Console.WriteLine("second pair contains first pair fully");
+                    continue;
+                    }
 
+	        }
 
-
-
+            Console.WriteLine("total: " + total);
+            Console.WriteLine("first: " + first);
+            Console.WriteLine("last: " + last);
 
             //keep console open
             Console.WriteLine("Hit any key to close this window...");
                 Console.ReadKey();
             
         }
-        private static int findValue(char c)
-        {
-            var valueString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            return valueString.IndexOf(c) + 1 ;
-        }
+        
         private static void readInput()
         {
             _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
