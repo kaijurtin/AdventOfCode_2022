6e56ff298703c4de7ed4a2c59a2205560db6e6e0
Kai Jurtin <kai@jurtin.de>
2022-12-08 22:57:46 +0100

    day8 round2 different solution

--- Program.cs
+++ Program.cs
@@ -12,143 +12,92 @@ namespace AdventOfCodeStartProject
         //each line in the list of strings represents one line in the txt file
         //copy the data you get on the adventofcode website into the txt file and start coding!
         private static List<string> _inputData;
-
+        
         static void Main(string[] args)
         {
-            //EXAMPLE CODE
-            //hit F5 to run it
-
-            //read data from input.txt and write it into inputdata field
             readInput();
-            //var intlist = _inputData.Select(i => int.Parse(i)).ToList();
-            //lets write the contents of the input.txt into the console
-
-            //Console.WriteLine("Content of input.txt:");
-            //_inputData.ForEach(i => Console.WriteLine(i));
-            //Console.WriteLine($"--END OF FILE--{Environment.NewLine}");
-            
-            //List<int> _inputData = _inputData.ConvertAll(item => int.Parse(item));
-            //List<Dictionary> main = new List<Dictionary>(); //this is the "/" dir
-            
-
-            var columns = _inputData.First().Length;
-            var rows = _inputData.Count;
-            var outertrees = columns * 2 + rows * 2 -4;
-            var fromLeft = 0;
-            var fromRight = 0;
-            var fromTop = 0;    
-            var fromBottom = 0;
-            bool stopLeft = false;
-            bool stopRight = false;
-            bool stopTop = false;
-            bool stopBottom = false;
-            var maxrow = new List<int>();
-            var maxcol = new List<int>();
+//var inputFile = File.ReadAllLines("../input/input_08.txt");
+        var input = _inputData;//new List<string>(inputFile);
 
 
-            int[,] data = new int[rows,columns]; 
-            int[,] result = new int[rows,columns];
 
-            for (int i = 0; i < rows; i++)
-			{
-            var line =_inputData[i].ToCharArray();
-                for (int j = 0; j < line.Length; j++)
-                {
-                    data[i,j] = Convert.ToInt32(line[j].ToString());
-                }
-			}
-            
-         //IEnumerable<int> allValues = data.Cast<int>();
 
-            for (int i = 1; i < rows-1; i++)
+         var grid = new List<List<int>>();
+            foreach (var line in input)
             {
-                var temp = new List<int>();
-                for (int j = 1; j < columns-1; j++)
-                {
-                    var value = (data[i,j]);
-                    temp.Add(value);
+                var row = new List<int>();
+
+                foreach(var tree in line){
+                    row.Add(Convert.ToInt32(tree));
                 }
-                maxrow.Add(temp.Max());
-                temp.Clear();
-                Console.WriteLine(maxrow[i-1]);
-            }
-            for (int i = 1; i < columns-1; i++)
-            {
-                var temp = new List<int>();
-                for (int j = 1; j < rows-1; j++)
-                {
-                    var value = (data[j,i]);
-                    temp.Add(value);
-                }
-                maxcol.Add(temp.Max());
-                temp.Clear();
-                Console.WriteLine(maxcol[i-1]);
+
+                grid.Add(row);
             }
 
+            int bestScenicScore = 0;
 
-
-var templeft = new List<int>();
-var tempright = new List<int>();
-var temptop = new List<int>();  
-var tempbottom = new List<int>();
-
-            for (int i = 1; i < rows-1; i++)
+            for (int row = 0; row < grid.Count; row++)
             {
-                for (int j = 0; j < columns-2; j++)
-			    {
-                    templeft.Add(data[i,j]);
-                    Console.WriteLine(data[i,j+1] +":"+ templeft.Max());
-                    if(data[i,j+1] > templeft.Max()) 
-                       result[i,j+1] = 1;
-                    
-                }
-                 Console.WriteLine("from Left: " +fromLeft);
-                templeft.Clear();
-			
-                for (int k = columns-1 ; k > 1; k--)
-			    {
-                    tempright.Add(data[i,k]);
-                    Console.WriteLine(data[i,k-1] +":"+ tempright.Max());
-                    if(data[i,k-1] > tempright.Max()) 
-                        result[i,k-1] = 1;
-                }
-                Console.WriteLine("from Right: " +fromRight);
-                tempright.Clear();
-
-            }
-            for (int i = 1; i < columns-1; i++)
-			{
-                for (int j = 0; j < rows-2; j++)
+                for (int col = 0; col < grid[row].Count; col++)
                 {
-                    temptop.Add(data[j,i]);
-                    Console.WriteLine(data[j+1,i] +":"+ temptop.Max());
-                    if(data[j+1,i] > temptop.Max())
-                        result[j+1,i] = 1;
+                    int currentHeight = grid[row][col];
+
+                    int left = 0;
+                    int right = 0;
+                    int up = 0;
+                    int down = 0;
+
+
+                    //left check
+                    for(int leftCol = col-1; leftCol >= 0; leftCol--)
+                    {
+                        left++;
+                        if(grid[row][leftCol] >= currentHeight){
+                            break;
+                        }
+                    }
+            
+                    //right check
+                    for(int rightCol = col+1; rightCol < grid[row].Count; rightCol++)
+                    {
+                        right++;
+                        if(grid[row][rightCol] >= currentHeight){
+                            break;
+                        }
+                    }
+            
+                    //up check
+                    for(int upRow = row-1; upRow >= 0; upRow--)
+                    {
+                        up++;
+                        if(grid[upRow][col] >= currentHeight){
+                            break;
+                        }
+                    }
+            
+                    //down check
+                    for(int downRow = row+1; downRow < grid.Count; downRow++)
+                    {
+                        down++;
+                        if(grid[downRow][col] >= currentHeight){
+                            break;
+                        }
+                    }
+
+                    int scenicScore = left * right * up * down;
+
+                    if (scenicScore > bestScenicScore)
+                    {
+                        bestScenicScore = scenicScore;
+                    }
                 }
-                Console.WriteLine("from Top: " +fromTop);
-                temptop.Clear();
-                for (int k = rows-1; k > 1; k--)
-			    {
-                    tempbottom.Add(data[k,i]);
-                    Console.WriteLine(data[k-1,i] +":"+ tempbottom.Max());
-                    if(data[k-1,i] > tempbottom.Max()) 
-                        result[k-1,i] = 1;
-			    }
-                Console.WriteLine("from Bottom: " +fromBottom);
-                tempbottom.Clear();
+            }
+            Console.WriteLine(bestScenicScore.ToString());
+            Console.ReadLine();
 
-			}
 
-            
-            var sum = result.Cast<int>().Sum() + outertrees;
-            
-            Console.WriteLine("SUM: " +  sum);
-            //keep console open
-            Console.WriteLine("Hit any key to close this window...");
-                Console.ReadKey();
-            
+
         }
-        
         private static void readInput()
         {
             _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
