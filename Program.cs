c0eb4b509f52194a33d1693ac900afe533ae35b6
Kai Jurtin <kai@jurtin.de>
2022-12-10 12:35:35 +0100

    day10 round1

--- Program.cs
+++ Program.cs
@@ -12,73 +12,51 @@ namespace AdventOfCodeStartProject
         //each line in the list of strings represents one line in the txt file
         //copy the data you get on the adventofcode website into the txt file and start coding!
         private static List<string> _inputData;
-
+        private static List<int> sums = new List<int>();
 
         static void Main(string[] args)
         {
             readInput();
-            var input = _inputData;
-            int xHead = 0;
-            int yHead = 0;
+            var cycle = 0;
+            var cc = 0;
+            var x = 1;
+            
 
-            var xTails = new int[9];
-            var yTails =  new int[9];
+            foreach(string l in _inputData)
+            {
+                //l.Split(' ');
+                if (l.Substring(0,4).Equals("noop"))
+                {
+                    cycle++;
+                    checkCycle(cycle,x);
+                    Console.WriteLine("cycle: " + cycle);
+                }
 
-            var positions = new HashSet<string>();
-            positions.Add(position(xHead,yHead));
+                if (l.Substring(0, 4).Equals("addx"))
+                {
+                    cycle++;
+                    checkCycle(cycle, x);
+                    cycle++;
+                    checkCycle(cycle, x);
+                    Console.WriteLine("cycle: " + cycle);
+                    x += Convert.ToInt32(l.Substring(5, l.Length-5));
+                    Console.WriteLine("X: " + x);
 
-            foreach (var line in input){
-                var instruction = line.Split(' ');
+                }
 
-                for (int i = Convert.ToInt32(instruction[1]); i > 0; i--){
-                    int prevX = xHead;
-                    int prevY = yHead;
 
-                    if (instruction[0] == "U"){
-                        yHead++;
-                    }
-                    else if (instruction[0] == "D"){
-                        yHead--;
-                    }
-                    else if (instruction[0] == "L"){
-                        xHead--;
-                    }
-                    else if (instruction[0] == "R"){
-                        xHead++;
-                    }
 
-                    for (int tail = 0; tail < 9; tail++){
-                        int xTail = xTails[tail];
-                        int yTail = yTails[tail];
 
-                        int xCompare = tail != 0 ? xTails[tail-1] : xHead;
-                        int yCompare = tail != 0 ? yTails[tail-1] : yHead;
 
-                        if (xCompare == xTail && (yCompare == yTail || Math.Abs(yCompare - yTail) == 1) || yCompare == yTail && Math.Abs(xCompare - xTail) == 1) continue;
-                        if (Math.Abs(yCompare - yTail) == 1 && Math.Abs(xCompare - xTail) == 1) continue;
 
-                        if (xCompare == xTail){
-                            yTails[tail] += yCompare > yTail ? 1 : -1;
-                        }
-                        else if (yCompare == yTail){
-                            xTails[tail] += xCompare > xTail ? 1 : -1;
-                        }                
-                        else {
-                            yTails[tail] += yCompare > yTail ? 1 : -1;
-                            xTails[tail] += xCompare > xTail ? 1 : -1;
-                        }
-                
-                    
-                        if (tail == 8){
-                            positions.Add(position(xTails[tail],yTails[tail]));
-                        }
-                    }
-                }        
+
+
             }
-
-            Console.WriteLine(positions.Count);
-
-
+            foreach (var item in sums)
+            {
+            Console.WriteLine("sums: " + item);
+            }
+            Console.WriteLine("total: " + sums.Sum());
             Console.WriteLine("ENDE");
             Console.ReadKey();
 
@@ -86,6 +64,15 @@ namespace AdventOfCodeStartProject
         private static void readInput()
         {
             _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
+        }
+        private static void checkCycle(int cycle, int x)
+        {
+            var cyclechecks = new List<int>() { 20, 60, 100, 140, 180, 220 };
+            if (cyclechecks.Contains(cycle))
+            {
+                var num = cycle * x;
+                sums.Add(num);
+            }
         }
         private static string position(int x, int y)
         {
