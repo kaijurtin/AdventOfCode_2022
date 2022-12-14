50a51ce2327104fb2c5a53435b3f062d8c208c9c
Kai Jurtin <kjurtin@husky.ca>
2022-12-14 13:36:07 +0100

    day12 round1and2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -12,150 +14,109 @@ namespace AdventOfCodeStartProject
         //this field contains whats written in your input.txt
         //each line in the list of strings represents one line in the txt file
         //copy the data you get on the adventofcode website into the txt file and start coding!
-        private static List<string> _inputData;
-        private static List<int> sums = new List<int>();
-        
+        // private static List<string> _inputData;
+
 
         static void Main(string[] args)
         {
-            readInput();
-            List <Monkey> monks = new List<Monkey>();
-            List<string>things = new List<string>();  
-            //get all monkeys
-            for (int i = 0; i < _inputData.Count; i=i+7)
+            //readInput();
+            //Console.WriteLine("ENDE");
+            //Console.ReadKey();
+
+            var stopwatch = Stopwatch.StartNew();
+
+            var data =
+                (from line in File.ReadAllLines("input.txt")
+                    where !string.IsNullOrWhiteSpace(line)
+                    select line.ToArray()).ToArray();
+
+            var start = (x: -1, y: -1);
+            var end = (x: -1, y: -1);
+
+            for (int x = 0; x < data.Length; ++x)
+            for (int y = 0; y < data[0].Length; ++y)
             {
-                    
-                //monkey class
-                    int num = Convert.ToInt32((_inputData[i].Split(' ')[1]).Replace(":", ""));
-                    monks.Add(new Monkey(num));
-                //items
-                    things = _inputData[i+1].Split(' ').ToList();
-                    things.RemoveRange(0, 4);
-                        foreach (var item in things)
-                        {
-                            var ite = Convert.ToInt32(item.Replace(",", ""));
-                            monks[num].items.Add(ite);
-                        }
-                    things.Clear();
-                //formula
-                    var form = _inputData[i+2].Split(' ').ToList();
-                    var formula = "item="+form[5]+form[6]+form[7];
-                        formula = formula.Replace("old", "item");
-                        monks[num].formula = formula;
-                        
-                //test
-                var test = monks[num].testnum = Convert.ToInt32(_inputData[i + 3].Split(' ').Last()); ;
-                //true
-                monks[num].montrue= Convert.ToInt32(_inputData[i + 4].Split(' ').Last());
-                //false
-                monks[num].monfalse = Convert.ToInt32(_inputData[i + 5].Split(' ').Last());
+                if (data[x][y] == 'S')
+                {
+                    start = (x, y);
+                    data[x][y] = 'a';
+                }
+
+                if (data[x][y] == 'E')
+                {
+                    end = (x, y);
+                    data[x][y] = 'z';
+                }
             }
 
-            //find new divisor
-            var divisor = monks.Select(m => m.testnum).Aggregate((a, b) => a * b);
-            Console.WriteLine("divisor: "+divisor);
-            //get a round done
-            for (int i = 0; i < 10000; i++)
+            Dictionary<(int, int), int> distanceCache = new Dictionary<(int, int), int>() { { end, 0 } };
+
+            void ComputeCost(int x, int y)
             {
-                foreach (var monk in monks)
+                if (!distanceCache.TryGetValue((x, y), out var currentCost))
+                    throw new InvalidOperationException($"Cannot compute cost for neighbors of {x},{y}");
+
+                void ComputeNeighbor(int newX, int newY)
                 {
-                    while (monk.items.Any())
+                    if (newX < 0 || newX >= data.Length || newY < 0 || newY >= data[0].Length)
+                        return;
+
+                    if (data[newX][newY] + 1 >= data[x][y])
                     {
-                        monk.inspects++;
-                        long newval = monkeyCalc(monk, monk.items.First());
-                        double calc = Math.Floor((double)newval % divisor);
-                        var newmonk = 0;
-                        if (calc % monk.testnum == 0)
-                            newmonk = monk.montrue;
-                        else newmonk = monk.monfalse;
-                        monks[newmonk].items.Add((int)calc);
-                        monk.items.Remove(monk.items.First());
+                        if (!distanceCache.TryGetValue((newX, newY), out var currentNeighborCost) ||
+                            currentNeighborCost > currentCost + 1)
+                        {
+                            distanceCache[(newX, newY)] = currentCost + 1;
+                            ComputeCost(newX, newY);
+                        }
                     }
                 }
+
+                ComputeNeighbor(x - 1, y);
+                ComputeNeighbor(x + 1, y);
+                ComputeNeighbor(x, y - 1);
+                ComputeNeighbor(x, y + 1);
             }
 
-            var mostinspects = monks.OrderByDescending(x => x.inspects).Take(2);
-            var result = mostinspects.First().inspects * mostinspects.Last().inspects;
+            ComputeCost(end.x, end.y);
 
+            long GetPart1() =>
+                distanceCache[(start.x, start.y)];
 
-            foreach (var monk in monks)
+            long GetPart2()
             {
-                Console.WriteLine("Monkey: " + monk.number);
-                foreach (var item in monk.items)
-                {
-                    Console.WriteLine("item: " + item);
-                }
-                Console.WriteLine("inspects: " + monk.inspects);
-                Console.WriteLine("formula: " + monk.formula);
-                Console.WriteLine("Test number: " + monk.testnum);
-                Console.WriteLine("if true: " + monk.montrue);
-                Console.WriteLine("if false: " + monk.monfalse);
-                Console.WriteLine("\n");
-                Console.WriteLine("RESULT: "+result);
-            }
-        
+                var result = long.MaxValue;
 
-            Console.WriteLine("ENDE");
+                for (int x = 0; x < data.Length; ++x)
+                for (int y = 0; y < data[0].Length; ++y)
+                    if (data[x][y] == 'a' && distanceCache.TryGetValue((x, y), out var potential))
+                        if (potential < result)
+                            result = potential;
+
+                return result;
+            }
+
+            Console.WriteLine($"[{stopwatch.Elapsed}] Pre-compute");
+
+            stopwatch = Stopwatch.StartNew();
+            var part1Result = GetPart1();
+            Console.WriteLine($"[{stopwatch.Elapsed}] Part 1: {part1Result}");
+
+            stopwatch = Stopwatch.StartNew();
+            var part2Result = GetPart2();
+            Console.WriteLine($"[{stopwatch.Elapsed}] Part 2: {part2Result}");
+
             Console.ReadKey();
 
         }
-        private static void readInput()
-        {
-            _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
-        }
 
-
-        private static long monkeyCalc(Monkey monkey, long item)
-        {
-            long result = 0;
-            //var nextMonkey = 0;
-           // var item = monkey.items.First();
-            var formula = monkey.formula;
-
-            if (formula.Contains("+"))
-            {
-                result = item + long.Parse(formula.Split('+').Last());
-            }
-            else
-            {
-                if (formula.Split('*').Last() != "item")
-                {
-                    result = item * long.Parse(formula.Split('*').Last());
-                }
-                else result = item * item;
-            }
-
-            return result;
-        }
-    }
-    
-     public class Monkey
-    {
-        public int num;
-
-        //public List<int> items = new List<int>();
-        //public static int calcNew(int item)
+        //private static void readInput()
         //{
-        //    item = item + formula;
+        //    _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
         //}
-        public Monkey(int num)
-        {
-            number = num;
-            items = new List<long>();
-            inspects = 0;
-            formula = "0";
-            montrue = 0;
-            monfalse = 0;
-            testnum = 0;
 
-        }
-        public  int number { get; set; }
-        public  List<long> items { get; set; }
-        public  string formula { get; set; }
-        public  long testnum { get; set; }
-        public  int montrue { get; set; }
-        public  int monfalse { get; set; }
-        public long inspects { get; set; }
+
+
     }
 }
     
